f48c38a68d38ded01f9e3d0b99e426f1a554eed8
Jeremy Holland <jeremy@jeremypholland.com>
2017-01-05 11:58:09 -0600

    enabling servie

--- manifests/infra.pp
+++ manifests/infra.pp
@@ -19,15 +19,15 @@
 class newrelic::infra {
   include ::newrelic::infra::install
   include ::newrelic::infra::config
-  # include ::newrelic::infra::service
+  include ::newrelic::infra::service
 
-  Class['::newrelic::infra::install'] -> Class['::newrelic::infra::config'] # ~> Class['::newrelic::infra::service']
+  Class['::newrelic::infra::install'] -> Class['::newrelic::infra::config'] ~> Class['::newrelic::infra::service']
 
   anchor {
     'newrelic::infra::begin':
       before => [Class['::newrelic::infra::install'],Class['::newrelic::infra::config']],
-      # notify => Class['::newrelic::infra::service'];
-      # 'newrelic::infra::end':
-      # 'nrequire => Class['::newrelic::infra::service'];
+      notify => Class['::newrelic::infra::service'];
+    'newrelic::infra::end':
+      nrequire => Class['::newrelic::infra::service'];
   }
 }
