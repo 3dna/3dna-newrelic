c30783ed514c3e1bb6fe94ddbf46c795c3269832
James Fryman <james@fryman.io>
2018-07-06 11:38:48 -0500

    Bump key id for new relic

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,7 +28,7 @@ class newrelic::install (
       'src'       => false,
     },
     key         => {
-      'id'        => '548C16BF',
+      'id'        => 'B60A3EC9BC013B9C23790EC8B31B29E5548C16BF',
       'source'    => 'https://download.newrelic.com/548C16BF.gpg',
     },
   }
